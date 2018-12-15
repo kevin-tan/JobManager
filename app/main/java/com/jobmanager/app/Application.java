e84611bab37bfe069eddc50f75d496f261ea966d
Kevin Tan <kevin.tan97@hotmail.com>
2018-12-15 17:34:40 -0500

    added spring to project

---
+++ app/main/java/com/jobmanager/app/Application.java
@@ -0,0 +1,18 @@
+package com.jobmanager.app;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Kevin Tan 2018-12-15
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
