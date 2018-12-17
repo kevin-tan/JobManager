2fef1e6818c7d4eac895a6503a0e3152489daa32
Kevin Tan <kevin.tan97@hotmail.com>
2018-12-17 13:54:23 -0500

    added docs

--- app/main/java/com/jobmanager/app/job/schedule/CRONSchedule.java
+++ app/main/java/com/jobmanager/app/job/schedule/CRONSchedule.java
@@ -3,22 +3,65 @@ package com.jobmanager.app.job.schedule;
 import lombok.Data;
 
 /**
- * CRONSchedule is a reusable class to set the CRON schedule for a {@link com.jobmanager.app.job.Job} to run on.
+ * CRONSchedule is used to set the CRON schedule for a {@link com.jobmanager.app.job.Job} to run on
  */
 @Data
 public class CRONSchedule {
 
+    /**
+     * Private static inner-class to hold the CRON data structure
+     */
+    private static class CRON {
+        Long minute;
+        Long hour;
+        Long dayOfMonth;
+        Long month;
+        Long dayOfWeek;
+
+        /**
+         * Create CRON with specified minute, hour, dayOfMoth, month and dayOfWeek
+         *
+         * @param minute     [0 - 59] long value
+         * @param hour       [0 - 23] long value
+         * @param dayOfMonth [1 - 31] long value
+         * @param month      [1 - 12] long value
+         * @param dayOfWeek  [0 - 6] long value
+         */
+        CRON(Long minute, Long hour, Long dayOfMonth, Long month, Long dayOfWeek) {
+            this.minute = minute;
+            this.hour = hour;
+            this.dayOfMonth = dayOfMonth;
+            this.month = month;
+            this.dayOfWeek = dayOfWeek;
+        }
+    }
+
+    /**
+     * To allow the building of a CRON schedule
+     */
+    public static CRON buildCronSchedule(Long minute, Long hour, Long dayOfMonth, Long month, Long dayOfWeek) {
+        return new CRON(minute, hour, dayOfMonth, month, dayOfWeek);
+    }
+
     /* Whether the job should only be ran once */
     private boolean runOnce;
 
+    /* CRON schedule for the job to run */
+    private CRON schedule;
+
+    /* Whether the job is running or awaiting to run
+       if true, running, else awaiting to run */
+    private boolean running;
+
     /**
-     * Create a CRONSchedule that will perform the job at a fixed rate, or once only.
+     * Create a CRONSchedule that will perform the job at a fixed rate, or once only
      *
      * @param runOnce if {@code true}, job will run once only,
-     *                all custom CRON schedule will be ignore.
+     *                all custom CRON schedule will be ignore ({@link #schedule} is null)
      */
-    public CRONSchedule(boolean runOnce) {
+    public CRONSchedule(boolean runOnce, CRON schedule) {
         this.runOnce = runOnce;
+        if (!runOnce) this.schedule = schedule;
+        this.running = false;
     }
-
 }
