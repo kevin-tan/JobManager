1944c755b09514fddcd9007193b493df59e3bb12
Kevin Tan <kevin.tan97@hotmail.com>
2019-01-05 12:07:40 -0500

    adding service for angular components for dashboard

--- ui/my-app/src/app/app.component.ts
+++ ui/my-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AppService} from "./app.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'JobManager Dashboard';
+
+  constructor(private appService: AppService, private routing: Router){}
+
+  ngOnInit(){
+
+  }
+
 }
